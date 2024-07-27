21c9d4c1e70890c709c3e1d4d560dcccb2cdc164
Anushka4559 <anushkabhagat777@gmail.com>
2024-07-27 13:12:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import navbar from './components/navbar'
+
+const App = () =>{
+  return (
+    <div>
+      <navbar/>
+       
+    </div>
+  )
+
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const navbar = () =>{
+    return (
+      <div>
+        <div>
+            <div>
+                <div>
+                    <span></span>
+                    <h1>Go Food</h1>
+                </div>
+                <nav>
+                    <Link>Home</Link>
+                </nav>
+            </div>
+        </div>
+         
+      </div>
+    )
+  
+  }
+
+
+
+export default navbar
